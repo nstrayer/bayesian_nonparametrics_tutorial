ef580d1e711731f53e6d9d10cb53a08295d4ff38
Nick Strayer <nick.strayer@gmail.com>
2018-10-01 18:26:03 +0000

    Simple first pass through the iteration loop is finished. Needs to be wrapped in a purrr function to go faster.

--- MCMC_Dirichlet.R
+++ MCMC_Dirichlet.R
@@ -24,13 +24,27 @@ cluster_center_prior <- function(){
   rmvnorm(n = 1, mean = c(0,0), sigma = Sigma*5) 
 }
 
-  
+
+
+n <- nrow(data)  
 num_initial_clusts <- 20
+# How many proposal clusters drawn from the prior do we have for each iteration. 
+proposal_clusters <- 3
+# Concentration parameter
+alpha <- 1.2
+scalar_mult_denom <- (n-1) + alpha
+
+
+
+# Function to draw random cluster centers from our prior. 
+draw_clusters <- function(num_clusts, id_start = 0){
+  1:num_clusts %>% 
+  map_df(~as_tibble(cluster_center_prior())) %>% 
+  mutate(id = id_start + (1:n()))
+}
 
 # Start by drawing some number of intial clusters from our prior
-clusters <- 1:num_initial_clusts %>% 
-  map_df(~as_tibble(cluster_center_prior())) %>% 
-  mutate(id = 1:n())
+clusters <- draw_clusters(num_initial_clusts) 
 
 # We start by randomly assigning points to a given starting cluster. 
 data_cluster_membership <- data %>% 
@@ -40,24 +54,50 @@ data_cluster_membership <- data %>%
 i <- 1
 
 
-# Need to add M more clusters to the clusters list before merging with the data list.
 # Then need to decide which cluster to join based on the acceptance algorithm 
 # Wrap up in a purrr function to make it fast and clean. 
 
 # take out the point we're looking at 
-current_point <- data_cluster_membership[1,c('V1', 'V2')]
+current_point <- data_cluster_membership[i,c('V1', 'V2')]
 
-# Look at data omitting the currently looked at point. 
-(data_cluster_membership[-1,]) %>% 
-  group_by(cluster) %>% 
-  summarise(n_in = n()) %>% 
-  inner_join(clusters, by = c('cluster' = 'id')) %>% 
-  mutate(
-    likelihood_from_cluster = map2_dbl(V1, V2, ~dmvnorm(current_point, c(.x,.y)))
+# Add the newly drawn clusters to cluster object
+new_clusters <- clusters %>% 
+  bind_rows(
+    draw_clusters(proposal_clusters, id_start = max(.$id))
   )
 
+# Look at data omitting the currently point. 
+cluster_likelihoods <- (data_cluster_membership[-1,]) %>% 
+  group_by(cluster) %>% 
+  summarise(n_in = n()) %>% 
+  full_join(new_clusters, by = c('cluster' = 'id')) %>% 
+  mutate(
+    scaler_mult = ifelse(is.na(n_in), (alpha/proposal_clusters), n_in)/scalar_mult_denom,
+    likelihood_from_cluster = map2_dbl(V1, V2, ~dmvnorm(current_point, c(.x,.y))),
+    un_normed_prob_of_choosing = scaler_mult * likelihood_from_cluster,
+    normed_prob_of_choosing = un_normed_prob_of_choosing / sum(un_normed_prob_of_choosing)
+  )
+
+# Plot of cluster likelihoods
+cluster_likelihoods %>% 
+  ggplot(aes(x = cluster, y = normed_prob_of_choosing)) +
+  geom_point()
+
+# cluster
+cluster_choice <- sample(
+  cluster_likelihoods$cluster, 
+  size = 1, 
+  prob = cluster_likelihoods$normed_prob_of_choosing )
+
+# Return a simple dataframe with the ID of the point and its cluster assignment. 
+tibble(
+  id = i, 
+  cluster = cluster_choice
+)
 
 
+# Mutate the cluster membership dataframe with this new value. 
+data_cluster_membership[i,'cluster'] <- cluster_choice
 
 # First we will initialize a cluster dataframe that contains info on each cluster, its center, and 
 
